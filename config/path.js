43444cf977e0f6b6dfc91a6d67e7e252fe82021e
ViolettaParamonova <violettapar1995@gmail.com>
2022-03-17 19:45:01 +0300

    html finish start css

--- config/path.js
+++ config/path.js
@@ -29,7 +29,7 @@ module.exports = {
     },
 
     img: {
-        src: pathSrc + "/img/*.{png,jpg,jpeg,gif,svg}",
+        src: pathSrc + "/img/**/*.{png,jpg,jpeg,gif,svg}",
         watch: pathSrc + "/img/**/*.{png,jpg,jpeg,gif,svg}",
         dest: pathDest + "/img"
     },
